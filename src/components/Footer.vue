2bf40f020c4f795eca337098b8e2ecb6e326b4fa
pulkit604 <guppul@gmail.com>
2023-04-06 23:52:32 +0200

    amend footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -53,57 +53,6 @@
             </div>
           </div>
         </div>
-        <div class="mt-12 xl:mt-0">
-          <h3 class="text-base font-medium text-white">Language &amp; Currency</h3>
-          <form class="mt-4 sm:max-w-xs">
-            <fieldset class="w-full">
-              <label for="language" class="sr-only">Language</label>
-              <div class="relative">
-                <select id="language" name="language" class="block w-full appearance-none rounded-md border border-transparent bg-gray-700 bg-none py-2 pl-3 pr-10 text-base text-white focus:border-white focus:outline-none focus:ring-white sm:text-sm">
-                  <option selected="">English</option>
-                  <option>French</option>
-                  <option>German</option>
-                  <option>Japanese</option>
-                  <option>Spanish</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
-                  <ChevronDownIcon class="h-4 w-4 text-white" aria-hidden="true" />
-                </div>
-              </div>
-            </fieldset>
-            <fieldset class="mt-4 w-full">
-              <label for="currency" class="sr-only">Currency</label>
-              <div class="relative mt-1.5">
-                <select id="currency" name="currency" class="block w-full appearance-none rounded-md border border-transparent bg-gray-700 bg-none py-2 pl-3 pr-10 text-base text-white focus:border-white focus:outline-none focus:ring-white sm:text-sm">
-                  <option>ARS</option>
-                  <option selected="">AUD</option>
-                  <option>CAD</option>
-                  <option>CHF</option>
-                  <option>EUR</option>
-                  <option>GBP</option>
-                  <option>JPY</option>
-                  <option>USD</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
-                  <ChevronDownIcon class="h-4 w-4 text-white" aria-hidden="true" />
-                </div>
-              </div>
-            </fieldset>
-          </form>
-        </div>
-      </div>
-      <div class="border-t border-gray-700 pt-8 lg:flex lg:items-center lg:justify-between xl:mt-0">
-        <div>
-          <h3 class="text-base font-medium text-white">Subscribe to our newsletter</h3>
-          <p class="mt-2 text-base text-gray-300">The latest news, articles, and resources, sent to your inbox weekly.</p>
-        </div>
-        <form class="mt-4 sm:flex sm:max-w-md lg:mt-0">
-          <label for="email-address" class="sr-only">Email address</label>
-          <input type="email" name="email-address" id="email-address" autocomplete="email" required="" class="w-full min-w-0 appearance-none rounded-md border border-transparent bg-white py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:border-white focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 sm:max-w-xs" placeholder="Enter your email" />
-          <div class="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-            <button type="submit" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800">Subscribe</button>
-          </div>
-        </form>
       </div>
       <div class="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
         <div class="flex space-x-6 md:order-2">
