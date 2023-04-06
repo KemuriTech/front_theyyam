a1bacfc01b063f6e87f59860afbfc3647d3ae284
pulkit604 <guppul@gmail.com>
2023-04-07 00:02:20 +0200

    link absolute

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -66,10 +66,10 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 const navigation = [
-  { name: 'Events', href: 'Events', current: true },
-  { name: 'Past Events', href: 'Pastevents', current: false },
-  { name: 'Map View', href: 'Mapview', current: false },
-  { name: 'Story', href: 'Story', current: false },
+  { name: 'Events', href: '/Events', current: true },
+  { name: 'Past Events', href: '/Pastevents', current: false },
+  { name: 'Map View', href: '/Mapview', current: false },
+  { name: 'Story', href: '/Story', current: false },
 ]
 </script>
 <style scoped>
