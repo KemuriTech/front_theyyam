56780e70886f86ffc9fa825ddb233fad61269b16
dipeshkemuri <dipesh@kemuri.in>
2023-06-02 15:33:20 +0530

    refactor: on phone make a hyper link to call

--- src/pages/Event[id].vue
+++ src/pages/Event[id].vue
@@ -46,14 +46,17 @@ const ContactInfo = {
         'contact2',
         'contact3'
     ],
-    template:`
-      <div v-if="name||designation||contact1||contact2||contact3" class="border-b pb-2 mb-3">
-        <span v-if="name">{{name}}</span> <span v-if="designation">({{designation}})</span>
-        <div v-if="contact1">{{contact1}}</div>
-        <div v-if="contact2">{{contact2}}</div>
-        <div v-if="contact3">{{contact3}}</div>
-      </div>
-    `
+    methods: {
+        getTel(tel) {
+            return `tel:${tel}`;
+        }
+    },
+    template:`<div v-if="name||designation||contact1||contact2||contact3" class="border-b pb-2 mb-3">
+    <span v-if="name">{{name}}</span> <span v-if="designation">({{designation}})</span>
+    <div v-if="contact1"><a :href="getTel(contact1)">{{contact1}}</a></div>
+    <div v-if="contact2"><a :href="getTel(contact2)">{{contact2}}</a></div>
+    <div v-if="contact3"><a :href="getTel(contact3)">{{contact3}}</a></div>
+    </div>`
 }
 const GetMainMedia = {
     setup() {
