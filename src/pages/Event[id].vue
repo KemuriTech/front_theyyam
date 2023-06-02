1c207a85dac726c5a08a7e7294e38493ff23edcf
dipeshkemuri <dipesh@kemuri.in>
2023-06-02 16:14:59 +0530

    style: no underline to tel

--- src/pages/Event[id].vue
+++ src/pages/Event[id].vue
@@ -53,9 +53,9 @@ const ContactInfo = {
     },
     template:`<div v-if="name||designation||contact1||contact2||contact3" class="border-b pb-2 mb-3">
     <span v-if="name">{{name}}</span> <span v-if="designation">({{designation}})</span>
-    <div v-if="contact1"><a :href="getTel(contact1)">{{contact1}}</a></div>
-    <div v-if="contact2"><a :href="getTel(contact2)">{{contact2}}</a></div>
-    <div v-if="contact3"><a :href="getTel(contact3)">{{contact3}}</a></div>
+    <div v-if="contact1"><a class="no-underline" :href="getTel(contact1)">{{contact1}}</a></div>
+    <div v-if="contact2"><a class="no-underline" :href="getTel(contact2)">{{contact2}}</a></div>
+    <div v-if="contact3"><a class="no-underline" :href="getTel(contact3)">{{contact3}}</a></div>
     </div>`
 }
 const GetMainMedia = {
