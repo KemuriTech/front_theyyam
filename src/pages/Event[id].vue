de8d1c4205dc0d3afb9a86083f4ac54118ccb306
dipeshkemuri <dipesh@kemuri.in>
2023-06-05 12:04:25 +0530

    style: remove color form link

--- src/pages/Event[id].vue
+++ src/pages/Event[id].vue
@@ -251,8 +251,11 @@ export default {
                                     <DisclosurePanel as="div" class="pb-6 prose prose-sm">
                                         <p>{{ eventData.ext_9 }}</p>
                                         <p class="pb-2">{{ eventData.ext_10 }}</p>
-                                        <a :href="`https://www.google.com/maps/search/?api=1&query=${eventData.ext_11}%2C${eventData.ext_12}`" target="_blank" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 no-underline">
-                                            Open in Google Maps
+                                        <a :href="`https://www.google.com/maps/search/?api=1&query=${eventData.ext_11}%2C${eventData.ext_12}`" target="_blank" class="text-xs py-2.5 text-center font-bold no-underline">
+                                            <span>
+                                                Open in Google Maps
+                                            <svg class="inline mb-0.5" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="12" height="12" viewBox="0 0 48 48" ><path d="M 40.960938 4.9804688 A 2.0002 2.0002 0 0 0 40.740234 5 L 28 5 A 2.0002 2.0002 0 1 0 28 9 L 36.171875 9 L 22.585938 22.585938 A 2.0002 2.0002 0 1 0 25.414062 25.414062 L 39 11.828125 L 39 20 A 2.0002 2.0002 0 1 0 43 20 L 43 7.2460938 A 2.0002 2.0002 0 0 0 40.960938 4.9804688 z M 12.5 8 C 8.3826878 8 5 11.382688 5 15.5 L 5 35.5 C 5 39.617312 8.3826878 43 12.5 43 L 32.5 43 C 36.617312 43 40 39.617312 40 35.5 L 40 26 A 2.0002 2.0002 0 1 0 36 26 L 36 35.5 C 36 37.446688 34.446688 39 32.5 39 L 12.5 39 C 10.553312 39 9 37.446688 9 35.5 L 9 15.5 C 9 13.553312 10.553312 12 12.5 12 L 22 12 A 2.0002 2.0002 0 1 0 22 8 L 12.5 8 z"/></svg>
+                                            </span>
                                         </a>
                                     </DisclosurePanel>
                                 </Disclosure>
