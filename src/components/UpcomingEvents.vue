48518ee0c4458b95163d699870f9188415060943
Soumyank-Kemuri <Soumyank@kemuri.in>
2023-06-12 13:59:15 +0530

    format

--- src/components/UpcomingEvents.vue
+++ src/components/UpcomingEvents.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="relative">
-    <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
-      <div class="bg-clip-left relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(100%-110rem)] sm:w-[72.1875rem]" />
+    <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+      aria-hidden="true">
+      <div
+        class="bg-clip-left relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(100%-110rem)] sm:w-[72.1875rem]" />
     </div>
   </div>
   <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <div  v-show="$route.path==='/'" class="text-center">
+    <div v-show="$route.path === '/'" class="text-center">
       <h2 class="text-base font-semibold tracking-wider text-indigo-600">Be Part of the Tradition</h2>
     </div>
     <div class="flex items-center justify-center space-x-4">
@@ -14,7 +16,7 @@
     <div class="flex items-center justify-end space-x-4">
       <a href="/events" class="whitespace-nowrap text-sm font-medium text-indigo-600 hover:text-indigo-500">View
         all<span aria-hidden="true">
-      <ArrowRightIcon class="h-5 w-5 inline-block -mt-1" aria-hidden="true" />
+          <ArrowRightIcon class="h-5 w-5 inline-block -mt-1" aria-hidden="true" />
         </span></a>
     </div>
     <div class="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
@@ -22,8 +24,10 @@
         <NuxtLink :to="`event${event.topics_id}`" class="cursor-pointer">
           <div class="w-full aspect-h-3 rounded bg-gray-100">
             <img v-if="!isAnyYTVideo(event)" :src="event.ext_16.url" class="object-center object-cover h-full w-full" />
-            <iframe v-else class='bg-cover bg-center rounded' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen  v-on:mouseover="playVideo" v-on:mouseleave="stopVideo" :data-uid='getYTUID(event)'
-                    :style='`background-image: url("${getYoutubeImage(event)}");`'></iframe>
+            <iframe v-else class='bg-cover bg-center rounded' frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen v-on:mouseover="playVideo" v-on:mouseleave="stopVideo" :data-uid='getYTUID(event)'
+              :style='`background-image: url("${getYoutubeImage(event)}");`'></iframe>
           </div>
           <div class="mt-4 flex  text-left justify-between text-base font-medium text-gray-900 space-x-8 mb-1">
             <h3>
@@ -32,7 +36,7 @@
             </h3>
           </div>
           <p class="mt-1 text-sm text-gray-500 flex items-center space-x-2">
-            <span class="mr-1">{{$formatDate(event.ext_4, event.ext_5)}}</span>
+            <span class="mr-1">{{ $formatDate(event.ext_4, event.ext_5) }}</span>
           </p>
         </NuxtLink>
       </div>
@@ -46,44 +50,44 @@ export default {
   async setup() {
 
     let upcomingEventData = {};
-    let {$formatDate} = useNuxtApp();
-    
-   await fetch(`https://theyyam.g.kuroco.app/rcms-api/1/latest-event?cnt=4`, {
+    let { $formatDate } = useNuxtApp();
+
+    await fetch(`https://theyyam.g.kuroco.app/rcms-api/1/latest-event?cnt=4`, {
       method: 'GET',
     }).then(response => response.json())
       .then(response => {
         upcomingEventData = response.list;
-    });
+      });
 
-    return { upcomingEventData,$formatDate};
+    return { upcomingEventData, $formatDate };
   },
   methods: {
-      playVideo(event) {
-          const _target = event.target;
+    playVideo(event) {
+      const _target = event.target;
 
-          _target.setAttribute('src', 'https://www.youtube.com/embed/'+_target.getAttribute('data-uid')+'?autoplay=1&mute=1&controls=0')
-      },
+      _target.setAttribute('src', 'https://www.youtube.com/embed/' + _target.getAttribute('data-uid') + '?autoplay=1&mute=1&controls=0')
+    },
 
-      getYoutubeImage(item) {
-          return `https://img.youtube.com/vi/${this.getYTUID(item)}/0.jpg`;
-      },
+    getYoutubeImage(item) {
+      return `https://img.youtube.com/vi/${this.getYTUID(item)}/0.jpg`;
+    },
 
-      stopVideo(event){
-          const _target = event.target;
+    stopVideo(event) {
+      const _target = event.target;
 
-          _target.setAttribute('src', '');
-      },
+      _target.setAttribute('src', '');
+    },
 
-      isAnyYTVideo(item){
-          if (item?.ext_13?.url || item?.ext_14?.url || item?.ext_15?.url || item?.ext_16?.url) return true;
-          return false;
-      },
-      splitYTUID(url) {
-          return url.split("v=")[1] ?? null;
-      },
-      getYTUID(item) {
-          return this.splitYTUID(item?.ext_13?.url) ?? this.splitYTUID(item?.ext_14?.url) ?? this.splitYTUID(item?.ext_15?.url) ?? this.splitYTUID(item?.ext_16?.url);
-      }
+    isAnyYTVideo(item) {
+      if (item?.ext_13?.url || item?.ext_14?.url || item?.ext_15?.url || item?.ext_16?.url) return true;
+      return false;
+    },
+    splitYTUID(url) {
+      return url.split("v=")[1] ?? null;
+    },
+    getYTUID(item) {
+      return this.splitYTUID(item?.ext_13?.url) ?? this.splitYTUID(item?.ext_14?.url) ?? this.splitYTUID(item?.ext_15?.url) ?? this.splitYTUID(item?.ext_16?.url);
+    }
   },
   components: {
     ArrowRightIcon,
