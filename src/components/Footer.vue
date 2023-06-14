6104ca1cc55f615798567d844890de1dfc0841ee
dipeshkemuri <131749655+dipeshkemuri@users.noreply.github.com>
2023-06-14 16:13:26 +0530

    Logo (#20)
    * refactor: logo changed n nuxtLink implemented to nav n footer
    
    * style-refactor: navbar style changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,9 @@
         <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
             <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
                 <div v-for="item in navigation.main" :key="item.name" class="px-5 py-2">
-                    <a :href="item.href" class="text-base text-white hover:text-gray-400">
+                    <NuxtLink :to="item.href" class="text-base text-white hover:text-gray-400">
                         {{ item.name }}
-                    </a>
+                    </NuxtLink>
                 </div>
             </nav>
             <p class="mt-8 text-center text-base text-gray-400">&copy; {{ navigation.date.year }} <a href="https://kemuri.in/" target="_blank" class="text-base text-white hover:text-gray-400">KEMURI Technology</a>. All rights reserved.</p>
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,12 @@
+
+const SERVICE_NAME = 'Theyyam Festival';
 const KUROCO_API_VER = 1;
 const KUROCO_API_PREFIX = 'rcms-api';
 const BASE_URL = 'theyyam.g.kuroco.app';
 const LANDING_PAGE_URL = `https://${BASE_URL}`;
 
 export {
+    SERVICE_NAME,
     KUROCO_API_VER,
     KUROCO_API_PREFIX,
     BASE_URL,
