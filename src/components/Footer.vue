e6302a50739382b123684bc20f50352b4c28ed88
dipeshkemuri <131749655+dipeshkemuri@users.noreply.github.com>
2023-06-19 11:10:52 +0530

    style: Footer ui and home link in footer (#23)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,23 @@
 <template>
-    <footer class="bg-gray-800">
+    <footer class="bg-white border-t" aria-labelledby="footer-heading">
         <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
             <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
                 <div v-for="item in navigation.main" :key="item.name" class="px-5 py-2">
-                    <NuxtLink :to="item.href" class="text-base text-white hover:text-gray-400">
+                    <NuxtLink :to="item.href" class="text-base font-medium text-gray-500 hover:text-gray-900">
                         {{ item.name }}
                     </NuxtLink>
                 </div>
             </nav>
-            <p class="mt-8 text-center text-base text-gray-400">&copy; {{ navigation.date.year }} <a href="https://kemuri.in/" target="_blank" class="text-base text-white hover:text-gray-400">KEMURI Technology</a>. All rights reserved.</p>
+            <p class="mt-8 text-center text-base text-gray-500">&copy; {{ navigation.date.year }} <a href="https://kemuri.in/" target="_blank" class="font-medium text-base text-gray-500 hover:text-gray-900">KEMURI Technology</a>. All rights reserved.</p>
         </div>
     </footer>
 </template>
 
 <script setup>
-import { defineComponent, h } from 'vue'
-import { ref } from 'vue'
-import { ChevronDownIcon } from '@heroicons/vue/20/solid'
-
-const enabled = ref(false)
 
 const navigation = {
     main: [
+        { name: 'Home', href: '/' },
         { name: 'Events', href: '/events' },
         { name: 'Past Events', href: '/pastevents' },
     ],
