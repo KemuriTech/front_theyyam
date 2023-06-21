8fe2535c63501454124af5f085d211f0d2e4308d
dipeshkemuri <131749655+dipeshkemuri@users.noreply.github.com>
2023-06-21 18:49:13 +0530

    Kuroco api over Algolia (#26)
    * refactor: algolia replaced by kuroco api
    
    * infinite scroll
    
    * refactor: add search feature
    
    * refactor: changes implemented

---
+++ src/components/ui/skeletons/EventCardSkeleton.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="mx-auto rounded w-72">
+            <div class="h-36 p-3 bg-gray-300 animate-pulse rounded-lg">
+            </div>
+            <div class="py-3">
+                <div class="grid grid-cols-3 gap-4 mt-2">
+                    <div class="h-6 col-span-3 bg-gray-300 rounded animate-pulse">
+                    </div>
+                    <div class="h-6 bg-gray-300 rounded animate-pulse">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -3,12 +3,16 @@ import APIModule from "~/repositories/apiModule";
 interface APIInstance {
     contact: APIModule,
     occasion: APIModule,
+    occasions: APIModule,
+    pastEvents: APIModule,
 }
 
 export default defineNuxtPlugin((nuxtApp) => {
     const modules: APIInstance = {
         contact: new APIModule('contact-us'),
         occasion: new APIModule('event'),
+        occasions: new APIModule('events'),
+        pastEvents: new APIModule('past-events'),
     };
 
     return {
