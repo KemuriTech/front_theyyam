4e9d7423e5f6c9317573971595702b9fc89204a5
Soumyank-Kemuri <112612692+Soumyank-Kemuri@users.noreply.github.com>
2023-06-30 17:00:40 +0530

    Add microsoft clarity (#50)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,15 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
+      script: [
+        { innerHTML: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "hkr0anyws4");
+        `, type: 'text/javascript' }
+      ],
       htmlAttrs: {
         lang: 'en'
       },
